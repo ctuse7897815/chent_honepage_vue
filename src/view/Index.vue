b78cd38ac03519bbb1d8b65cf0193c5d618b1262
ctuse <ctuse7897815@gmail.com>
2017-08-16 17:52:49 +0800

    index

---
+++ src/view/Index.vue
@@ -0,0 +1,18 @@
+<style>
+
+</style>
+<template>
+  <div>
+    msg: {{ msg }}
+  </div>
+</template>
+<script>
+  export default{
+    name: 'index',
+    data () {
+      return {
+        msg: '123'
+      }
+    }
+  }
+</script>
